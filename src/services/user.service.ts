e63f03e04175f4321d74715d69e360f336d18b91
Dratasy <thdquan.book@gmail.com>
2025-07-24 22:06:35 +0700

    video 61

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -19,18 +19,8 @@ const handleCreateUser = async (
 }
 
 const getAllUsers = async () => {
-    const connection = await getConnection();
-    // A simple SELECT query
-    try {
-        const [results, fields] = await connection.query(
-            'SELECT * FROM `User`'
-        );
-
-        return results
-    } catch (err) {
-        console.log(err);
-    }
-    return [];
+    const users = await prisma.user.findMany();
+    return users;
 }
 
 const handleDeleteUser = async (id: string) => {
