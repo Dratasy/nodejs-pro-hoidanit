aa8d2ac6799eb1ad75d2b7bd5fd4b79a3b60a57c
Dratasy <thdquan.book@gmail.com>
2025-07-24 22:14:31 +0700

    video 62

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -38,29 +38,27 @@ const handleDeleteUser = async (id: string) => {
 }
 
 const getUserById = async (id: string) => {
-    try {
-        const connection = await getConnection();
-        const sql = 'SELECT * FROM `users` WHERE `id` = ?';
-        const values = [id];
-        const [results, fields] = await connection.execute(sql, values);
-        return results[0];
-    } catch (err) {
-        console.log(err);
-        return [];
-    }
+    const user = await prisma.user.findUnique({
+        where: {
+            id: +id
+        }
+    });
+    return user;
 }
 
 const updateUserById = async (id: string, fullName: string, email: string, address: string) => {
-    try {
-        const connection = await getConnection();
-        const sql = 'UPDATE `users` SET `name` = ?, `email` = ?, `address` = ? WHERE `id` = ?';
-        const values = [fullName, email, address, id];
-        const [result, fields] = await connection.execute(sql, values);
-        return result;
-    } catch (err) {
-        console.log(err);
-        return [];
-    }
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: +id
+        },
+        data: {
+            name: fullName,
+            email: email,
+            address: address
+        }
+    })
+
+    return updatedUser;
 }
 
 export {
