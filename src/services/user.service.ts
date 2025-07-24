6096a5049a45ca040c70b97dbbe8c087a1dc989d
Dratasy <thdquan.book@gmail.com>
2025-07-24 12:04:16 +0700

    video 48

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,22 @@
 import getConnection from '../config/database';
 
-const handleCreateUser = (
+const handleCreateUser = async(
     fullName: string,
     email: string,
     address: string) => {
         //insert into database 
+        const connection = await getConnection();
 
-        //return result
+        try {
+            const sql = 'INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)';
+            const values = [fullName, email, address];
 
-        console.log(`create user: ${fullName} - ${email} - ${address}`);
+            const [result, fields] = await connection.execute(sql, values);
+            return result;
+        } catch (err) {
+            console.log(err);
+            return [];
+        }
 }
 
 const getAllUsers = async () => {
