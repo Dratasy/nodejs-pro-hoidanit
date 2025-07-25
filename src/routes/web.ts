c35d83367a48036af1fa322cb364cba40d33c6f1
Dratasy <thdquan.book@gmail.com>
2025-07-25 22:30:44 +0700

    video 75

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,15 +1,19 @@
 import express, { Express } from 'express';
 import { getHomePage, getCreateUserPage, postCreateUser, postDeleteUser, getViewUser, postUpdateUser } from 'controllers/user.controller';
+import { getDashboardPage } from 'controllers/dashboard.controller';
 
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
-    router.get('/', getHomePage );
+    router.get('/', getHomePage);
     router.get('/create-user', getCreateUserPage);
     router.post('/handle-create-user', postCreateUser)
     router.post('/handle-delete-user/:id', postDeleteUser)
     router.get('/handle-view-user/:id', getViewUser)
     router.post('/handle-update-user', postUpdateUser)
+
+    //admin routes
+    router.get('/admin', getDashboardPage);
 
     app.use('/', router);
 }
