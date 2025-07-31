820ba492e60f56bab9ad9b26c6544a3f4a6aa014
Dratasy <thdquan.book@gmail.com>
2025-07-31 21:31:29 +0700

    video 84

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -1,21 +1,26 @@
+import { hashPassword } from "services/user.service";
 import { prisma } from "./client"
+import { ACCOUNT_TYPE } from "./constant";
 
 
 const initDatabase = async () => {
     const countUser = await prisma.user.count();
     const countRole = await prisma.role.count();
     if (countUser == 0) {
+        const defaultPassword = await hashPassword("123456");
         await prisma.user.createMany({
             data: [
-                {
+                {   
+                    fullName: "hoidanit",
                     username: "hoidanit@gmail.com",
-                    password: "123456",
-                    accountType: "SYSTEM"
+                    password: defaultPassword,
+                    accountType: ACCOUNT_TYPE.SYSTEM
                 },
                 {
+                    fullName: "admin",
                     username: "admin@gmail.com",
-                    password: "123456",
-                    accountType: "SYSTEM"
+                    password: defaultPassword,
+                    accountType: ACCOUNT_TYPE.SYSTEM
                 }
             ]
         })
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,13 @@
 import { prisma } from 'config/client';
 import { ACCOUNT_TYPE } from 'config/constant';
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
+const hashPassword = async (plainText: string) => {
+    const hash = await bcrypt.hash(plainText, saltRounds);
+    return hash;
+}
 
 const handleCreateUser = async (
     fullName: string,
@@ -8,13 +16,13 @@ const handleCreateUser = async (
     phone: string,
     avatar: string
 ) => {
-
+    const defaultPassword = await hashPassword("123456");
     const newUser = await prisma.user.create({
         data: {
             fullName: fullName,
             username: email,
             address: address,
-            password: "123456",
+            password: defaultPassword,
             accountType: ACCOUNT_TYPE.SYSTEM,
             avatar: avatar,
             phone: phone
@@ -77,5 +85,6 @@ export {
     handleDeleteUser,
     getUserById,
     updateUserById,
-    getAllRoles
+    getAllRoles,
+    hashPassword
 };
