67a4a3c5c1c8ffd58d878e00329d7cff3e91d089
Dratasy <thdquan.book@gmail.com>
2025-08-01 21:20:08 +0700

    video 90

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,14 +2,8 @@ import { Request, Response } from 'express';
 import { handleCreateUser, getAllUsers, handleDeleteUser, getUserById, updateUserById, getAllRoles } from 'services/user.service';
 
 const getHomePage = async (req: Request, res: Response) => {
-    const users = await getAllUsers();
-    return res.render(
-        'home',
-        {
-            users: users,
-        }
 
-    );
+    return res.render(`client/home/show.ejs`);
 }
 
 const getCreateUserPage = async (req: Request, res: Response) => {
