c407384293be9519088818066751bd9d7d03c8a1
Dratasy <thdquan.book@gmail.com>
2025-08-13 17:45:36 +0700

    video 147

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { countTotalProductClientPages, getProducts } from 'services/client/item.service';
-import { userFilter, yeucau1, yeucau2, yeucau3, yeucau4, yeucau5, yeucau6, yeucau7 } from 'services/client/product.filter';
+import { getProductWithFilter, userFilter, yeucau1, yeucau2, yeucau3, yeucau4, yeucau5, yeucau6, yeucau7 } from 'services/client/product.filter';
 import { handleCreateUser, getAllUsers, handleDeleteUser, getUserById, updateUserById, getAllRoles } from 'services/user.service';
 
 const getHomePage = async (req: Request, res: Response) => {
@@ -17,12 +17,23 @@ const getHomePage = async (req: Request, res: Response) => {
 }
 
 const getProductFilterPage = async (req: Request, res: Response) => {
-    const { page } = req.query;
+    const { page, factory = "", target = "", price = "", sort = "" }
+        = req.query as {
+            page?: string,
+            factory: string,
+            target: string,
+            price: string,
+            sort: string
+        };
+
+
     let currentPage = page ? +page : 1;
     if (currentPage <= 0) currentPage = 1;
-    const totalPages = await countTotalProductClientPages(6);
+    // const totalPages = await countTotalProductClientPages(6);
 
-    const products = await getProducts(currentPage, 6);
+    // const products = await getProducts(currentPage, 6);
+
+    const { products, totalPages } = await getProductWithFilter(currentPage, 6, factory, target, price, sort);
     return res.render("client/home/filter.ejs", {
         products,
         totalPages: +totalPages,
